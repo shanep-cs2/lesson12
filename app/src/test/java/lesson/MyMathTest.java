6b5a0e35db5c9461b663f392ae7e63e82de0eb6a
shane panter <shane@foundationcode.com>
2022-05-20 11:00:10 -0600

    Updated tests

--- app/src/test/java/lesson/MyMathTest.java
+++ app/src/test/java/lesson/MyMathTest.java
@@ -5,6 +5,40 @@ import static org.testng.Assert.*;
 
 public class MyMathTest {
 
-  //TODO
+  @Test
+  public void testGoodInput(){
+      int actual = MyMath.divide("10", "2");
+      assertEquals(actual, 5);
+  }
+
+  @Test(expectedExceptions = {ArithmeticException.class})
+  public void testDivideByZero(){
+      MyMath.divide("10", "0");
+  }
+
+  @Test(expectedExceptions = {NumberFormatException.class})
+  public void testFloat(){
+      MyMath.divide("10.2", "2");
+  }
+
+  @Test(expectedExceptions = {NumberFormatException.class})
+  public void testFloat2(){
+      MyMath.divide("10", "2/2");
+  }
+
+  @Test(expectedExceptions = {NumberFormatException.class})
+  public void testBadInput(){
+      MyMath.divide("foo", "bar");
+  }
+
+  @Test(expectedExceptions = {NumberFormatException.class})
+  public void testEmptyString(){
+      MyMath.divide("", "");
+  }
+
+  @Test(expectedExceptions = {NumberFormatException.class})
+  public void testNullString(){
+      MyMath.divide(null, null);
+  }
     
 }
